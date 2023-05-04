a874e47fd942420ce6da1eed2afb69d2f3310f63
Kyle Lahnakoski <kyle@lahnakoski.com>
2023-05-04 06:08:06 -0400

    update version number

--- packaging/setup.py
+++ packaging/setup.py
@@ -15,5 +15,5 @@ setup(
     name='mo-times',
     packages=["mo_times/vendor/dateutil/zoneinfo","mo_times/vendor/dateutil","mo_times/vendor","mo_times"],
     url='https://github.com/klahnakoski/mo-times',
-    version='5.378.23124'
+    version='5.381.23124'
 )
